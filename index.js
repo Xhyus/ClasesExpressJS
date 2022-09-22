43a3afed5cd72543da25cc5bc73ff55a2262d112
Ignacio González <ignacio.gonzalez1901@alumnos.ubiobio.cl>
2022-09-21 21:47:53 -0300

    Inicializacion y linkeo a base de datos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const cors = require('cors');
 require('dotenv').config();
 
@@ -8,21 +8,22 @@ app.use(cors())
 app.use(express.json())
 app.options('*', cors())
 
-// const options = {
-//     useNewUrlParser: true,
-//     autoIndex: true,
-//     keepAlive: true,
-//     connectTimeoutMS: 10000,
-//     socketTimeoutMS: 45000,
-//     family: 4,
-//     useUnifiedTopology: true
-// }
+const options = {
+    useNewUrlParser: true,
+    autoIndex: true,
+    keepAlive: true,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family: 4,
+    useUnifiedTopology: true
+}
 
-// mongoose.connect(process.env.URL, options, function (error) {
-//     if (error) {
-//         console.log(error)
-//     }
-// })
+mongoose.connect(process.env.DB, options, function (error) {
+    if (error) {
+        console.log(error)
+    }
+    console.log("Se ha conectado de forma correcta a la base de datos")
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
